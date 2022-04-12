3b272863d6ad83b248aa80df9b7c55e45e2eda07
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-04-13 00:10:24 +0600

    Change brand name

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ import './Header.css';
 const Header = () => {
     return (
         <nav className='header'>
-            <div className='nav-brand'>Theme Shop</div>
+            <div className='nav-brand'>Online Shop</div>
             <div>
                 <Link to="/shop">Shop</Link>
                 <Link to="/orders">Orders</Link>
